30cdb6f34f1efde5d6340a5c3ca7c177dd95cfd6
tellefk <tellef94@hotmail.com>
2022-10-04 23:18:13 +0200

    added reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,9 +2,13 @@ import classes from "./HeaderCartButton.module.css"
 
 import CartIcon from "../Cart/CartIcon"
 
+import CartContext from "../../store/cart-context"
 
+import { useContext } from "react"
 
 const HeaderCartButton=(props) =>{
+
+    const cartCtx=useContext(CartContext)
     return (
         <button className={classes.button} onClick={props.onClickShowCart}>
             <span className={classes.icon}>
@@ -12,7 +16,7 @@ const HeaderCartButton=(props) =>{
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {cartCtx.items.length}
             </span>
         </button>
     )
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,9 +1,36 @@
 
 import CartContext from "./cart-context"
 
+import { useReducer } from "react"
+
+const defaultCartState={
+    items:[],
+    totalAmount:0
+
+}
+
+
+const cartReducer=(state,action)=>{
+    if (action.type === "ADD"){
+        const updatedItems=state.items.concat(action.item)
+        const updatedTotalamount=state.totalAmount+action.item.amount*action.item.price
+        return {
+            items:updatedItems,
+            totalAmount:updatedTotalamount
+        }
+    }
+    return defaultCartState
+}
+
 const CartProvider=props=>{
-    const addItemHandler=item=>{}
-    const removeItemHandler=item=>{}
+    const [cartState,dispatchCartAction]=useReducer(cartReducer,defaultCartState)
+
+    const addItemHandler=item=>{
+        dispatchCartAction({type:"ADD",item:item})
+    }
+    const removeItemHandler=item=>{
+        dispatchCartAction({type:"REMOVE",item:item})
+    }
 
     const cartContext={
         items:[],
