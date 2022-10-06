0a5fd89c45764a02637f7d65b5fb5d9cc1e063dd
tellefk <tellef94@hotmail.com>
2022-10-06 23:29:26 +0200

    done with part 11

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,19 +4,30 @@ import CartIcon from "../Cart/CartIcon"
 
 import CartContext from "../../store/cart-context"
 
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 
-const HeaderCartButton=(props) =>{
+const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setbtnIsHighlighted] = useState(false)
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`
 
-    const cartCtx=useContext(CartContext)
+    const { items } = useContext(CartContext)
+    useEffect(() => {
+        if (items.length === 0) return
+        setbtnIsHighlighted(true)
+
+        const timer = setTimeout(() => {
+            setbtnIsHighlighted(false)
+        }, 300)
+        return () => { clearTimeout(timer) }
+    }, [items])
     return (
-        <button className={classes.button} onClick={props.onClickShowCart}>
+        <button className={btnClasses} onClick={props.onClickShowCart}>
             <span className={classes.icon}>
-                <CartIcon/>
+                <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                {cartCtx.items.length}
+                {items.length}
             </span>
         </button>
     )
